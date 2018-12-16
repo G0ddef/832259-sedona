c20b7c50cfb17136e5e2fc53fa2d9bb020147ec9
G0ddef <goddef@mail.ru>
2018-12-17 00:26:02 +0200

    Изменил отступы, переименовал файлы

--- js/app.js
+++ js/app.js
@@ -4,17 +4,17 @@ var arrival = document.getElementById("arrival-date");
 var departure = document.getElementById("date-departure");
 var adults = document.getElementById("adults");
 
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    searchform.classList.toggle("form-show");
-    arrival.focus();
-  });
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchform.classList.toggle("form-show");
+  arrival.focus();
+});
 
-  searchform.addEventListener("submit", function (evt) {
-    if (!arrival.value || !departure.value || !adults.value) {
-      evt.preventDefault();
-      arrival.classList.add("form-error")
-      departure.classList.add("form-error")
-      adults.classList.add("form-error")
-    }
-  });
+searchform.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adults.value) {
+    evt.preventDefault();
+    arrival.classList.add("form-error")
+    departure.classList.add("form-error")
+    adults.classList.add("form-error")
+  }
+});
